309ddf14d4ee10be7fd54ca3743d6dba9fcdf0b4
Jiahuan He <jiahuan_he@outlook.com>
2017-03-07 21:59:37 -0600

    New GUI :( discard previous design.

--- Draft11/Draft11/TodoItemTableViewCell.swift
+++ Draft11/Draft11/TodoItemTableViewCell.swift
@@ -15,33 +15,8 @@ import UIKit
 class TodoItemTableViewCell: UITableViewCell, UITextViewDelegate {
     
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var expandedView: UIView!
-    @IBOutlet weak var setDueView: UIView!
     
     @IBOutlet weak var stackViewHeightConstraint: NSLayoutConstraint!
-    var expandedViewHeight: CGFloat{
-        get{
-            if expandedView.isHidden{
-                return 0
-            }
-            else{
-                return CGFloat(28)
-            }
-        }
-    }
-    
-    var setDueViewHeight: CGFloat{
-        get{
-            if setDueView.isHidden{
-                return 0
-            }
-            else{
-                return CGFloat(28)
-            }
-        }
-    }
-    
-    
     
     var delegate: TodoItemTableViewCellDelegate?
     
@@ -51,18 +26,22 @@ class TodoItemTableViewCell: UITableViewCell, UITextViewDelegate {
         let screenSize: CGRect = UIScreen.main.bounds
         let toobar = toolbarView(frame: CGRect(x: 0, y:0 , width: screenSize.width, height: 30))
         
-        let redButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        let dueLabel = UILabel(frame: CGRect(x: screenSize.width/2+50, y: 0, width: 60, height: 30))
+        dueLabel.text = "Due"
+        
+        let redButton = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 30))
         redButton.setTitle("red", for: .normal)
-//        
-//        let yellowButton = UIButton(frame: CGRect(x: 30, y: 0, width: 30, height: 30))
-//        redButton.setTitle("yellow", for: .normal)
-//        
+        
+        let yellowButton = UIButton(frame: CGRect(x: 50, y: 0, width: 60, height: 30))
+        yellowButton.setTitle("yellow", for: .normal)
+//
 //        let greenButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
 //        redButton.setTitle("green", for: .normal)
         
         
         toobar.addSubview(redButton)
-//        toobar.addSubview(yellowButton)
+        toobar.addSubview(yellowButton)
+        toobar.addSubview(dueLabel)
 //        toobar.addSubview(greenButton)
         
         
@@ -71,12 +50,11 @@ class TodoItemTableViewCell: UITableViewCell, UITextViewDelegate {
         
         
         
-        expandedView.isHidden = true
-        setDueView.isHidden = true
+
         
         textView.returnKeyType = UIReturnKeyType.done
         
-        stackViewHeightConstraint.constant = textView.sizeThatFits(textView.frame.size).height + expandedViewHeight + setDueViewHeight
+        stackViewHeightConstraint.constant = textView.sizeThatFits(textView.frame.size).height
         textView.delegate = self
         
         textView.inputAccessoryView = toobar
@@ -91,14 +69,7 @@ class TodoItemTableViewCell: UITableViewCell, UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if text == "\n"{
             
-            if !expandedView.isHidden {
-                expandedView.isHidden = true
-            }
-            if !setDueView.isHidden {
-                setDueView.isHidden = true
-            }
             adjustHeightConstrant()
-            
             textView.resignFirstResponder()
             return false
         }
@@ -106,17 +77,14 @@ class TodoItemTableViewCell: UITableViewCell, UITextViewDelegate {
     }
     
     private func adjustHeightConstrant(){
-        stackViewHeightConstraint.constant = textView.sizeThatFits(textView.frame.size).height + expandedViewHeight + setDueViewHeight
+        stackViewHeightConstraint.constant = textView.sizeThatFits(textView.frame.size).height
         delegate!.cellHeightDidChange(cell: self)
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
-        expandedView.isHidden = !expandedView.isHidden
-        setDueView.isHidden = !setDueView.isHidden
         
         adjustHeightConstrant()
         
-        print(stackViewHeightConstraint.constant," = ",textView.sizeThatFits(textView.frame.size).height,"+", expandedViewHeight)
     }
     
 
